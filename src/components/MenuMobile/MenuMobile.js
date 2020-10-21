4379ea49836c89562208200791e17b5cbd9cb725
Kishor Budhathoki <budhathokikishor10@gmail.com>
2020-10-21 20:34:24 +0200

    Changing design of mobile nav

---
+++ src/components/MenuMobile/MenuMobile.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import classes from "./MenuMobile.module.css";
+// import
+
+function MenuMobile() {
+  return <div className={classes.MenuMobile}></div>;
+}
+
+export default MenuMobile;
